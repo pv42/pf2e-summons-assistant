114532672b5bf4ee62602151d65548761fdf8cf4
cadowtin <chasdowtin@gmail.com>
2025-07-14 17:49:29 -0500

    necro work

--- scripts/module.js
+++ scripts/module.js
@@ -1,6 +1,7 @@
 import { CREATURES, MODULE_ID, SOURCE, SUMMON_LEVELS_BY_RANK } from "./const.js";
 import { extractDCValueRegex, incarnateDetails, isIncarnate } from "./incarnate.js";
 import { setupSettings } from "./settings.js";
+import { getSpecificSummonDetails } from "./specificSummons.js";
 
 Hooks.once("init", async function () {
   setupSettings();
@@ -186,58 +187,6 @@ function getTraditionalSummonerSpellDetails(uuid, rank) {
       return null;
   }
   return details;
-}
-
-function getSpecificSummonDetails(uuid, data = { rank: 0, summonerLevel: 0, dc: 0 }) {
-  switch (uuid) {
-    case SOURCE.SUMMON.PHANTASMAL_MINION:
-      return { specific_uuids: [CREATURES.PHANTASMAL_MINION], rank: data.rank }
-    case SOURCE.MISC.LIGHT:
-      if (hasNoTargets()) {
-        return {
-          specific_uuids: Object.values(CREATURES.LIGHT), rank: data.rank, modifications: {
-            "level": data.rank
-          }
-        }
-      }
-      else return null;
-
-    case SOURCE.INCARNATE.SUMMON_HEALING_SERVITOR:
-      return incarnateDetails({
-        uuids: [CREATURES.HEALING_SERVITOR],
-        rank: data.rank,
-        dc: data.dc
-      })
-    case SOURCE.INCARNATE.TEMPEST_OF_SHADES:
-      return incarnateDetails({
-        uuids: [CREATURES.TEMPEST_OF_SHADES],
-        rank: data.rank,
-        dc: data.dc
-      })
-    case SOURCE.MISC.CALL_URSINE_ALLY:
-      if (data.summonerLevel < 10) {
-        return { specific_uuids: [CREATURES.BLACK_BEAR], rank: 3 }
-      } else if (data.summonerLevel < 12) {
-        return { specific_uuids: [CREATURES.GRIZZLY_BEAR], rank: 4 }
-      } else if (data.summonerLevel < 14) {
-        return { specific_uuids: [CREATURES.POLAR_BEAR], rank: 5 }
-      } else {
-        return { specific_uuids: [CREATURES.CAVE_BEAR], rank: 6 }
-      }
-
-    case SOURCE.NECROMANCER.CREATE_THRALL:
-      return { specific_uuids: [CREATURES.NECROMANCER.THRALL], rank: data.rank }
-    case SOURCE.NECROMANCER.PERFECTED_THRALL:
-      return { specific_uuids: [CREATURES.NECROMANCER.PERFECTED_THRALL], rank: data.rank }
-    case SOURCE.NECROMANCER.SKELETAL_LANCERS:
-      return { specific_uuids: [CREATURES.NECROMANCER.SKELETAL_LANCERS], rank: data.rank, amount: 5 }
-    case SOURCE.NECROMANCER.LIVING_GRAVEYARD:
-      return { specific_uuids: [CREATURES.NECROMANCER.LIVING_GRAVEYARD], rank: data.rank }
-    case SOURCE.NECROMANCER.RECURRING_NIGHTMARE:
-      return { specific_uuids: [CREATURES.NECROMANCER.RECURRING_NIGHTMARE], rank: data.rank }
-    default:
-      return null;
-  }
 }
 
 
---
+++ scripts/specificSummons.js
@@ -0,0 +1,53 @@
+import { CREATURES, SOURCE } from "./const.js";
+
+export function getSpecificSummonDetails(uuid, data = { rank: 0, summonerLevel: 0, dc: 0 }) {
+    switch (uuid) {
+        case SOURCE.SUMMON.PHANTASMAL_MINION:
+            return { specific_uuids: [CREATURES.PHANTASMAL_MINION], rank: data.rank }
+        case SOURCE.MISC.LIGHT:
+            if (hasNoTargets()) {
+                return {
+                    specific_uuids: Object.values(CREATURES.LIGHT), rank: data.rank, modifications: {
+                        "level": data.rank
+                    }
+                }
+            }
+            else return null;
+
+        case SOURCE.INCARNATE.SUMMON_HEALING_SERVITOR:
+            return incarnateDetails({
+                uuids: [CREATURES.HEALING_SERVITOR],
+                rank: data.rank,
+                dc: data.dc
+            })
+        case SOURCE.INCARNATE.TEMPEST_OF_SHADES:
+            return incarnateDetails({
+                uuids: [CREATURES.TEMPEST_OF_SHADES],
+                rank: data.rank,
+                dc: data.dc
+            })
+        case SOURCE.MISC.CALL_URSINE_ALLY:
+            if (data.summonerLevel < 10) {
+                return { specific_uuids: [CREATURES.BLACK_BEAR], rank: 3 }
+            } else if (data.summonerLevel < 12) {
+                return { specific_uuids: [CREATURES.GRIZZLY_BEAR], rank: 4 }
+            } else if (data.summonerLevel < 14) {
+                return { specific_uuids: [CREATURES.POLAR_BEAR], rank: 5 }
+            } else {
+                return { specific_uuids: [CREATURES.CAVE_BEAR], rank: 6 }
+            }
+
+        case SOURCE.NECROMANCER.CREATE_THRALL:
+            return { specific_uuids: [CREATURES.NECROMANCER.THRALL], rank: data.rank }
+        case SOURCE.NECROMANCER.PERFECTED_THRALL:
+            return { specific_uuids: [CREATURES.NECROMANCER.PERFECTED_THRALL], rank: data.rank }
+        case SOURCE.NECROMANCER.SKELETAL_LANCERS:
+            return { specific_uuids: [CREATURES.NECROMANCER.SKELETAL_LANCERS], rank: data.rank, amount: 5 }
+        case SOURCE.NECROMANCER.LIVING_GRAVEYARD:
+            return { specific_uuids: [CREATURES.NECROMANCER.LIVING_GRAVEYARD], rank: data.rank }
+        case SOURCE.NECROMANCER.RECURRING_NIGHTMARE:
+            return { specific_uuids: [CREATURES.NECROMANCER.RECURRING_NIGHTMARE], rank: data.rank }
+        default:
+            return null;
+    }
+}
