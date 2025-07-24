19a25bd928c475d949e5d5b362f1cb1231a99c20
Suldrun45 <49712710+Suldrun45@users.noreply.github.com>
2025-07-24 18:48:42 +0200

    Fix Manifest Eidolon (#35)

--- scripts/specificClasses/summoner.js
+++ scripts/specificClasses/summoner.js
@@ -14,24 +14,21 @@ export function setSummonerRelevantInfo(summonerActor, spellRelevantInfo) {
 export async function getEidolon(summonerActorId){
     const summonerActor = game.actors.get(summonerActorId);
     const defaultEidolonUUID = summonerActor.getFlag(MODULE_ID, 'eidolonUUID');
+
     const eidolons = game.actors
         .filter(act=> 
             act.type == 'character' && 
-            act.class.sourceId.compendiumSource == EIDOLON_CLASS_UUID
+            act.class.sourceId == EIDOLON_CLASS_UUID 
         )
         .map(act => ({ name: act.name, uuid: act.uuid, selected: defaultEidolonUUID && act.uuid == defaultEidolonUUID }));
-    
+  
     //TODO: When the Shared Data option of Toolbelt is back, check if there are any eidolons whose master are the summoner and, in that case, return them
-
     if (eidolons.length == 1){
         const eidolonUUID = eidolons[0].uuid;
         await summonerActor.setFlag(MODULE_ID, 'eidolonUUID', eidolonUUID);   
         return eidolonUUID;
-    }
-   ui.notifications.warn(game.i18n.localize("pf2e-summons-assistant.notification.summoner.too-many-eidelons"))
-   const eidolonUUID = eidolons[0].uuid;
-   await summonerActor.setFlag(MODULE_ID, 'eidolonUUID', eidolonUUID);   
-   return eidolonUUID;
+    } else if (eidolons.length > 1){
+        ui.notifications.warn(game.i18n.localize("pf2e-summons-assistant.notification.summoner.too-many-eidelons"));  
     }
     return null;
 }
